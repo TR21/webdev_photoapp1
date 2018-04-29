ee2a5c0509797615eb21a477238535390df49eb3
Rails App <tyriegel21@gmail.com>
2018-04-29 20:25:18 +0000

    image uploader

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'devise'
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
 gem 'stripe'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'fog'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
          has_one :payment
+         has_many :images
          accepts_nested_attributes_for :payment
 end
