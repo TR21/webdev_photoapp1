4b49bcd0eb12b509c3834c55843c5acedea68122
Rails App <tyriegel21@gmail.com>
2018-04-30 23:45:35 +0000

    renaming text

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :images
   devise_for :users, :controllers => {:registrations => 'registrations' }
   root 'welcome#index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
